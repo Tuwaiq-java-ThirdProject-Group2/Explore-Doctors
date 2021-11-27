7d205267cb99e2df6c6a0142d78f4514f0a478c3
thuriabdullah <thiii302081@gmail.com>
2021-11-27 21:23:08 +0300

    contract

--- demo/src/main/java/com/example/demo/Contract/Contract.java
+++ demo/src/main/java/com/example/demo/Contract/Contract.java
@@ -1,10 +1,54 @@
 package com.example.demo.Contract;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "Contract")
 public class Contract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ContractId;
+    private Date start_date;
+    private Date end_date;
+    private String owner;
+    public Contract(){}
+    public Contract(long contractId, Date start_date, Date end_date, String owner) {
+        ContractId = contractId;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.owner = owner;
+    }
 
+    public long getContractId() {
+        return ContractId;
+    }
+
+    public void setContractId(long contractId) {
+        ContractId = contractId;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
--- demo/src/main/java/com/example/demo/Contract/ContractRepository.java
+++ demo/src/main/java/com/example/demo/Contract/ContractRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.Contract;
 
-public class ContractRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContractRepository extends JpaRepository<Contract,Long> {
 }
