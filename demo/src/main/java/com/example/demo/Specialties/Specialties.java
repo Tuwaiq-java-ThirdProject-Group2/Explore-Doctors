44ec19818ebfd1db0fc2b59b3e41564fcb770317
aaldosari1 <dosr@live.com>
2021-11-27 15:30:48 +0300

    wrote the specialties code

--- demo/src/main/java/com/example/demo/Specialties/Specialties.java
+++ demo/src/main/java/com/example/demo/Specialties/Specialties.java
@@ -1,4 +1,56 @@
 package com.example.demo.Specialties;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "specialties")
 public class Specialties {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long SpecialtyId;
+    private String SpecialtyName;
+    private String specialtyCode;
+
+    public Specialties() {
+
+    }
+
+    public Specialties(long specialtiesId, String specialtyName, String specialtyCode) {
+        this.SpecialtyId = specialtiesId;
+        this.SpecialtyName = specialtyName;
+        this.specialtyCode = specialtyCode;
+    }
+
+    public void setSpecialtiesId(long specialtiesId) {
+        this.SpecialtyId = specialtiesId;
+    }
+
+    public long getSpecialtiesId() {
+        return SpecialtyId;
+    }
+
+    public void setSpecialtyName(String specialtyName) {
+        this.SpecialtyName = specialtyName;
+    }
+
+    public String getSpecialtyName() {
+        return SpecialtyName;
+    }
+
+    public void setSpecialtyCode(String specialtyCode) {
+        this.specialtyCode = specialtyCode;
+    }
+
+    public String getSpecialtyCode() {
+        return specialtyCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Specialties{" +
+                "id=" + SpecialtyId +
+                ", specialtyName='" + SpecialtyName + '\'' +
+                ", specialtyCode='" + specialtyCode + '\'' +
+                '}';
+    }
 }
--- demo/src/main/java/com/example/demo/Specialties/SpecialtiesServies.java
+++ demo/src/main/java/com/example/demo/Specialties/SpecialtiesServies.java
@@ -1,4 +1,39 @@
 package com.example.demo.Specialties;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
 public class SpecialtiesServies {
+    private final SpecialtiesRepository specialtiesRepository;
+    @Autowired
+    public SpecialtiesServies(SpecialtiesRepository specialtiesRepository){this.specialtiesRepository=specialtiesRepository;}
+    public List<Specialties> getSpecialtiess(){
+        return specialtiesRepository.findAll();
+    }
+
+    public Specialties getSpecialties(String id){
+        Long Specialties_id=Long.parseLong(id);
+        return specialtiesRepository.findById(Specialties_id).orElse(null);
+    }
+
+    public Specialties createSpecialties(Specialties specialties){
+        return specialtiesRepository.save(specialties);
+    }
+
+    public void deleteSpecialties(String id){
+        Long Specialties_id = Long.parseLong(id);
+        specialtiesRepository.deleteById(Specialties_id);
+    }
+
+    public void updateSpecialties(String id, Specialties data){
+        Long Specialties_id = Long.parseLong(id);
+        Specialties specialties = specialtiesRepository.findById(Specialties_id).orElse(null);
+
+        if (specialties != null) {
+            specialties.setSpecialtyName(data.getSpecialtyName());
+            specialties.setSpecialtyCode(data.getSpecialtyCode());
+            specialtiesRepository.save(specialties);
+        }
+    }
 }
